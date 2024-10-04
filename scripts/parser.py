01f78c13f4d13b12451a03f3bd6e4a2cbe7a0cab
Tianshuo Zhou <tianshuo.chou@gmail.com>
2024-10-03 19:12:23 -0700

    extend two ends of motif plotstr.

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 import argparse 
 
@@ -37,7 +38,7 @@ def shrink(i, j, bpairs, SIZE = 2):
 def replace_substring(string, start, end, replacement):
     return string[:start] + replacement + string[end:]
 
-
+# intial version without extending the outmost pair
 def get_mplotstr2(m):
     # shrink y
     # ynew = m['y_sub']
@@ -109,6 +110,78 @@ def get_mplotstr2(m):
     plotstr = ",".join([m['id']+"_m", ynew, greenplot + whiteplot + pairplot, poststr])
     return plotstr
 
+# extend the outmost pair with two unpaired bases
+def get_mplotstr(m):
+    # shrink y
+    # ynew = m['y_sub']
+    m['bpairs'] = sorted(m['bpairs'])
+    m['ipairs'] = sorted(m['ipairs'])
+    if m['bpairs'][0][0] >= 0:
+        ysub = m['y_star'][m['bpairs'][0][0]: m['bpairs'][0][1]+1]
+        ynew = m['y_star'][m['bpairs'][0][0]: m['bpairs'][0][1]+1]
+        start = m['bpairs'][0][0]
+    else:
+        ysub = m['y_star']
+        ynew = m['y_star']
+        # m['bpairs'][0] = [0, len(ysub)-1]
+        start = 0
+    # print(m['bpairs'])
+    # print(m['ipairs'])
+    # print(ynew)
+    SIZE = 3
+    delta = 0
+    # start = m['bpairs'][0][0]
+    for pair in m['bpairs'][1:]:
+        # print(pair, pair[1] - pair[0] - 1 - 2)
+        # print(len(ynew))
+        ynew = replace_substring(ynew, pair[0]+1-start-delta, pair[1]-start-delta, "...")
+        # print(len(ynew))
+        delta += pair[1] - pair[0] - 1 - SIZE
+        # print(f"delta: {delta}")
+        # print(f"ydiff: {len(m['y_sub']) - len(ynew)}")
+        assert len(ysub) - len(ynew) == delta, f"{len(ysub), len(ynew)}"
+        # print()
+    # shrink pairs
+    bpairs = []
+    ipairs = []
+    for i, j in m['bpairs']:
+        if i >= 0:
+            bpairs.append(shrink(i, j, m['bpairs'][1:], SIZE))
+        else:
+            bpairs.append((i, j))
+    for i, j in m['ipairs']:
+        ipairs.append(shrink(i, j, m['bpairs'][1:], SIZE))
+    i0, j0 = bpairs[0]
+    i_base = i0
+    flag = False
+    if i0 >= 0:
+        ynew = "." + ynew + "." # extend the outmost pair with two unpaired bases
+        i_base -= 1
+        greenplot = f"{i0+1-i_base} {j0+1-i_base} GREEN Fomark "
+        flag = True
+    else:
+        assert len(m['y_star']) == len(m['y_sub'])
+        greenplot = f"{1} {len(ynew)} GREEN Fomark "
+        i_base = 0
+    whiteplot = ""
+    for i, j in bpairs[1:]:
+        whiteplot += f"{i+1-i_base} {j+1-i_base} WHITE Fomark "
+    pairplot = ""
+    if bpairs[0][0] >= 0:
+        pairplot += f"{i0+1-i_base} {j0+1-i_base} 0.667 0.5 colorpair "
+    for i, j in bpairs[1:]:
+        pairplot += f"{i+1-i_base} {j+1-i_base} 0.667 0.5 colorpair "
+    for i, j in ipairs:
+        pairplot += f"{i+1-i_base} {j+1-i_base} 0.1667 1.0 colorpair "
+    poststr = ""
+    for i, j in bpairs[1:]:
+        poststr += f"{i+1-i_base+1} {j+1-i_base-1} 10 WHITE omark "
+    if flag:
+        poststr += f"1 1 10 WHITE omark "
+        poststr += f"{len(ynew)} {len(ynew)} 10 WHITE omark "
+    plotstr = ",".join([m['id']+"_m", ynew, greenplot + whiteplot + pairplot, poststr])
+    return plotstr
+
 
 def get_motifs(file):
     motifs_list = []
@@ -121,16 +194,17 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", '-p', type=str, default='9families_free/telomerase_ref.txt.pn.log.20240923174211.txt')
+    parser.add_argument("--extend", '-e', action='store_true')
 
     args = parser.parse_args()
     print('args:')
     print(args)
 
+    motf2plotstr = get_mplotstr # get_mplotstr2 if not args.extend else get_mplotstr
     json_motifs = get_motifs(args.path)
     plot_lines = []
     for im, motif in enumerate(json_motifs):
-        # print(im)
-        plot_lines.append('"'+get_mplotstr2(motif)+'"'+"\n")
+        plot_lines.append('"'+motf2plotstr(motif)+'"'+"\n")
     path_output = os.path.basename(args.path).replace('log', 'plotstr')
     with open(path_output, 'w') as f:
         f.writelines(plot_lines)
